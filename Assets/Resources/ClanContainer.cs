0a4bdb6b981f6ad9a9f32080747f0c4d613b06f4
Luke Vang <Lukvaaj@gmail.com>
2016-10-02 14:55:27 -0500

    Added code and example for clan selection

---
+++ Assets/Resources/ClanContainer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+
+[XmlRoot("ClanCollection")]
+public class ClanContainer {
+
+    [XmlArray("Clans")]
+    [XmlArrayItem("Clan")]
+    public List<Clan> clans = new List<Clan>();
+
+    public static ClanContainer Load(string path)
+    {
+        TextAsset _xml = Resources.Load<TextAsset>(path);
+
+        XmlSerializer serilizer = new XmlSerializer(typeof(ClanContainer));
+
+        StringReader reader = new StringReader(_xml.text);
+
+        ClanContainer clans = serilizer.Deserialize(reader) as ClanContainer;
+
+        reader.Close();
+
+        return clans;
+    }
+
+	
+}
